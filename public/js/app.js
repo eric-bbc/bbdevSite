6268fd05d8389460fb8da8794d9067fbc9fd9b59
janine-bbc <janine@blackandblackcreative.com>
2018-02-08 16:09:26 -0500

    adding sidebar on casestudy page

--- public/js/app.js
+++ public/js/app.js
@@ -7,3 +7,24 @@ $(document).ready(function($) {
         keyboardNavEnabled: true
     });
 });
+
+/* =========================================================================
+    Body Class
+========================================================================= */
+
+
+// $(document).on('click', '.js-body-add-class', function() {
+//     $('body').addClass($(this).data('body-class'));
+// });
+//
+// $(document).on('click', '.js-body-remove-class', function() {
+//     $('body').removeClass($(this).data('body-class'));
+// });
+//
+// $(document).on('click', '.js-body-toggle-class', function() {
+//     $('body').toggleClass($(this).data('body-class'));
+// });
+
+$('.desktop-view-more-trigger').on('click', function() {
+  $('.view-more').toggleClass('view-more-open')
+})
